78bceaaa988395835b00eb2dc9d48f36fe370be7
Nikita Pletnev <betpezeh1@yahoo.com>
2022-09-03 10:45:10 +0500

    add Example 002

---
+++ Example002/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число c");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+
+if (a > b)
+if (a > c)
+{
+    max = a;
+}
+if (b > a)
+if (b > c) 
+{
+    max = b;
+}
+if (c > b)
+if (c > a) 
+{
+    max = c;
+}
+    Console.WriteLine("Максимальное число " + max);
