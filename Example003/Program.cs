f2c1fe1a610293bd5b7d7382e3e83f94dc586e22
Nikita Pletnev <betpezeh1@yahoo.com>
2022-09-03 10:54:47 +0500

    add Example 003

---
+++ Example003/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+    Console.WriteLine(a +" является четным");
+    else
+    {
+        Console.WriteLine(a +" не является четным");
+    }
